52a495273614090811ab785ee924d5629b2f65a2
Benjamin Godfrey <benjamin.godfrey.dev@gmail.com>
2026-06-09 17:51:55 +0100

    feat: add bot

---
+++ src/bot.ts
@@ -0,0 +1,26 @@
+import { Telegraf, Context } from 'telegraf';
+import { message } from 'telegraf/filters';
+
+export interface TaskHandler {
+    handle(task: string): Promise<void>;
+}
+
+type TextMessageContext = Context & { message: { text: string } };
+
+export class Bot {
+    bot: Telegraf;
+    taskHandler: TaskHandler;
+
+    constructor(botToken: string, taskHandler: TaskHandler) {
+        this.bot = new Telegraf(botToken);
+        this.taskHandler = taskHandler;
+
+        this.bot.on(message('text'), this.handleTelegramMessage.bind(this));
+
+        this.bot.launch();
+    }
+
+    async handleTelegramMessage(ctx: TextMessageContext): Promise<void> {
+        await this.taskHandler.handle(ctx.message.text);
+    }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,15 @@
-const message: string = "hello, world!"
+import { Bot, TaskHandler } from './bot';
+import dotenv from 'dotenv';
 
-console.log(message);
+dotenv.config();
+const telegramBotKey = process.env.TELEGRAM_BOT_KEY || ''; 
+
+class TaskLogger implements TaskHandler {
+    handle(task: string): Promise<void> {
+        console.log(`Task revieved: ${task}`)
+        return Promise.resolve();
+    }
+}
+
+const taskLogger = new TaskLogger();
+const bot = new Bot(telegramBotKey, taskLogger)
